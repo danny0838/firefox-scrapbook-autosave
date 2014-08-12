f350071610011bc2f6b9780125e09e26bc3b2246
Danny Lin <danny0838@gmail.com>
2014-08-12 14:36:42 +0800

    Unify linefeeds and spaces.

--- chrome/content/browserOverlay.js
+++ chrome/content/browserOverlay.js
@@ -6,101 +6,101 @@
 
 var sbAutoSaveService = {
 
-	CapturedURLs : {},
+    CapturedURLs : {},
 
-	init : function()
-	{
-		gBrowser.addEventListener("load", function(aEvent){ sbAutoSaveService.handleBrowserLoad(aEvent); }, true);
-	},
+    init : function()
+    {
+        gBrowser.addEventListener("load", function(aEvent){ sbAutoSaveService.handleBrowserLoad(aEvent); }, true);
+    },
 
-	handleBrowserLoad : function(aEvent)
-	{
-		if ( !sbAutoSaveUtils.getBoolPref("enabled", true) ) {
-			return;
-		}
+    handleBrowserLoad : function(aEvent)
+    {
+        if ( !sbAutoSaveUtils.getBoolPref("enabled", true) ) {
+            return;
+        }
 
-		var win = aEvent.originalTarget.defaultView;
-		if ( win != win.top ) {
-			return;
-		}
+        var win = aEvent.originalTarget.defaultView;
+        if ( win != win.top ) {
+            return;
+        }
 
-		var url = win.location.href;
-		var idx = url.indexOf('#');
-		if (idx >= 0) {
-			url = url.slice(0, idx);
-		}
+        var url = win.location.href;
+        var idx = url.indexOf('#');
+        if (idx >= 0) {
+            url = url.slice(0, idx);
+        }
 
-		if ( url in this.CapturedURLs ) {
-			return;
-		}
-		if ( url.indexOf("http") !== 0 ) {
-			return;
-		}
+        if ( url in this.CapturedURLs ) {
+            return;
+        }
+        if ( url.indexOf("http") !== 0 ) {
+            return;
+        }
 
-		try {
-			var exclude = sbAutoSaveUtils.copyUnicharPref("exclude", "");
-			if (exclude != "") {
-				var regex = new RegExp(exclude, "i");
-				if (win.location.href.search(regex) >= 0) {
-					return;
-				}
-			}
-		}
-		catch(ex) {}
+        try {
+            var exclude = sbAutoSaveUtils.copyUnicharPref("exclude", "");
+            if (exclude != "") {
+                var regex = new RegExp(exclude, "i");
+                if (win.location.href.search(regex) >= 0) {
+                    return;
+                }
+            }
+        }
+        catch(ex) {}
 
-		var ts = sbAutoSaveCommon.getTimeStamp();
-		var monthStamp = ts.substring(0,6) + "00000000";
-		var DirURI = "urn:scrapbook:item" + monthStamp;
-		var fItem, fRes;
-		if ( !sbAutoSaveData.exists(sbAutoSaveCommon.RDF.GetResource(DirURI)) )
-		{
-			fItem = sbAutoSaveCommon.newItem(monthStamp);
-			monthStamp.match(/^(\d{4})(\d{2})\d{8}$/);
-			fItem.title = RegExp.$1 + "/" + RegExp.$2;
-			fItem.type = "folder";
-			fRes = sbAutoSaveData.addItem(fItem, "urn:scrapbook:root", 0);
-			sbAutoSaveData.createEmptySeq(fRes.Value);
-		}
+        var ts = sbAutoSaveCommon.getTimeStamp();
+        var monthStamp = ts.substring(0,6) + "00000000";
+        var DirURI = "urn:scrapbook:item" + monthStamp;
+        var fItem, fRes;
+        if ( !sbAutoSaveData.exists(sbAutoSaveCommon.RDF.GetResource(DirURI)) )
+        {
+            fItem = sbAutoSaveCommon.newItem(monthStamp);
+            monthStamp.match(/^(\d{4})(\d{2})\d{8}$/);
+            fItem.title = RegExp.$1 + "/" + RegExp.$2;
+            fItem.type = "folder";
+            fRes = sbAutoSaveData.addItem(fItem, "urn:scrapbook:root", 0);
+            sbAutoSaveData.createEmptySeq(fRes.Value);
+        }
 
-		var timeStamp = ts.substring(0,8) + "000000";
-		var targetURI = "urn:scrapbook:item" + timeStamp;
-		if ( !sbAutoSaveData.exists(sbAutoSaveCommon.RDF.GetResource(targetURI)) )
-		{
-			fItem = sbAutoSaveCommon.newItem(timeStamp);
-			timeStamp.match(/^(\d{4})(\d{2})(\d{2})\d{6}$/);
-			fItem.title = (new Date(parseInt(RegExp.$1, 10), parseInt(RegExp.$2, 10) - 1, parseInt(RegExp.$3, 10))).toLocaleDateString();
-			fItem.type = "folder";
-			fRes = sbAutoSaveData.addItem(fItem, DirURI, 0);
-			sbAutoSaveData.createEmptySeq(fRes.Value);
-		}
+        var timeStamp = ts.substring(0,8) + "000000";
+        var targetURI = "urn:scrapbook:item" + timeStamp;
+        if ( !sbAutoSaveData.exists(sbAutoSaveCommon.RDF.GetResource(targetURI)) )
+        {
+            fItem = sbAutoSaveCommon.newItem(timeStamp);
+            timeStamp.match(/^(\d{4})(\d{2})(\d{2})\d{6}$/);
+            fItem.title = (new Date(parseInt(RegExp.$1, 10), parseInt(RegExp.$2, 10) - 1, parseInt(RegExp.$3, 10))).toLocaleDateString();
+            fItem.type = "folder";
+            fRes = sbAutoSaveData.addItem(fItem, DirURI, 0);
+            sbAutoSaveData.createEmptySeq(fRes.Value);
+        }
 
-		var presetData = [
-			null,
-			null,
-			{
-				"images" : sbAutoSaveUtils.getBoolPref("images", true),
-				"styles" : sbAutoSaveUtils.getBoolPref("styles", true),
-				"script" : sbAutoSaveUtils.getBoolPref("script", false)
-			},
-			null,
-			null
-		];
+        var presetData = [
+            null,
+            null,
+            {
+                "images" : sbAutoSaveUtils.getBoolPref("images", true),
+                "styles" : sbAutoSaveUtils.getBoolPref("styles", true),
+                "script" : sbAutoSaveUtils.getBoolPref("script", false)
+            },
+            null,
+            null
+        ];
 
-		if (sbAutoSaveUtils.getBoolPref("ignore", false)) {
-			var sv = window.alert;
-			try {
-				window.alert = function(s){ return s; };
-				sbContentSaver.captureWindow(win, false, false, targetURI, 0, presetData, null);
-			}
-			finally {
-				window.alert = sv;
-			}
-		} else {
-			sbContentSaver.captureWindow(win, false, false, targetURI, 0, presetData, null);
-		}
+        if (sbAutoSaveUtils.getBoolPref("ignore", false)) {
+            var sv = window.alert;
+            try {
+                window.alert = function(s){ return s; };
+                sbContentSaver.captureWindow(win, false, false, targetURI, 0, presetData, null);
+            }
+            finally {
+                window.alert = sv;
+            }
+        } else {
+            sbContentSaver.captureWindow(win, false, false, targetURI, 0, presetData, null);
+        }
 
-		this.CapturedURLs[url] = true;
-	}
+        this.CapturedURLs[url] = true;
+    }
 };
 
 window.addEventListener("load", sbAutoSaveService.init, false);
